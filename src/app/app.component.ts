46188ed5344e910d93d7f29bcbbbd5dc1c01f028
null <null>
2024-09-29 06:11:46 +0200

    Test lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,66 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {TuiIcon} from '@taiga-ui/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {TuiButton, TuiCalendar, TuiDataList, TuiDropdown, TuiFlagPipe, TuiIcon, TuiTextfield} from '@taiga-ui/core';
+import {TuiLanguageSwitcherService} from '@taiga-ui/i18n/utils';
+import type {TuiCountryIsoCode, TuiLanguageName} from '@taiga-ui/i18n/types';
+import { TuiButtonSelect, TuiTile } from '@taiga-ui/kit';
+import { NgForOf, TitleCasePipe } from '@angular/common';
 
 @Component({
     standalone: true,
     selector: 'app',
-    imports: [TuiIcon],
+    imports: [TuiIcon, TuiCalendar, TuiButton, TuiFlagPipe, TuiTile,         NgForOf,
+        ReactiveFormsModule,
+        TitleCasePipe,
+        TuiButton,
+        TuiButtonSelect,
+        TuiDataList,
+        TuiDropdown,
+        TuiFlagPipe,
+        TuiTextfield,
+],
     templateUrl: './app.component.html',
     styleUrl: './app.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+    protected readonly switcher = inject(TuiLanguageSwitcherService);
+    protected readonly language = new FormControl(capitalize(this.switcher.language));
+ 
+    protected open = false;
+ 
+    public readonly flags = new Map<TuiLanguageName, TuiCountryIsoCode>([
+        ['belarusian', 'BY'],
+        ['chinese', 'CN'],
+        ['dutch', 'NL'],
+        ['english', 'GB'],
+        ['french', 'FR'],
+        ['german', 'DE'],
+        ['hebrew', 'IL'],
+        ['italian', 'IT'],
+        ['japan', 'JP'],
+        ['kazakh', 'KZ'],
+        ['korean', 'KR'],
+        ['malay', 'MY'],
+        ['polish', 'PL'],
+        ['portuguese', 'PT'],
+        ['russian', 'RU'],
+        ['spanish', 'ES'],
+        ['turkish', 'TR'],
+        ['ukrainian', 'UA'],
+        ['vietnamese', 'VN'],
+    ]);
+ 
+    public readonly names: TuiLanguageName[] = Array.from(this.flags.keys());
+ 
+    public setLang(lang: TuiLanguageName): void {
+        this.language.setValue(lang);
+        this.switcher.setLanguage(lang);
+        this.open = false;
+    }
+
+}
+
+function capitalize(value: string): string {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ import type {ApplicationConfig} from '@angular/core';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
+import { tuiLanguageSwitcher } from '@taiga-ui/i18n/utils';
+import type { TuiLanguageName} from '@taiga-ui/i18n/types';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -19,5 +21,18 @@ export const appConfig: ApplicationConfig = {
                 'Terry Jones',
             ],
         },
+        tuiLanguageSwitcher(
+            /**
+             * @note:
+             * then the i18n language files will be loaded from node_modules
+             */
+            async (language: TuiLanguageName): Promise<unknown> =>
+              import(
+                /* webpackMode: "lazy" */
+                /* webpackChunkName: "i18n-lazy-" */
+                `@taiga-ui/i18n/languages/${language}`
+                // also you can override the paths to your i18n language files
+              ),
+          ),
     ],
 };
